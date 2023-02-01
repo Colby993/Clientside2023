c1b43c35198d9840b9b063bc4e64d57cd5afed14
Colby993 <122412306+Colby993@users.noreply.github.com>
2023-01-31 21:31:32 -0600

    Add files via upload

---
+++ Chapter 1/caseProject/Animals.js
@@ -0,0 +1,28 @@
+let Bear = "Bear.jpg"
+let Wolf = "Wolf.jpg"
+let Alligator = "Alligator.jpg"
+let Cheetah = "Cheetah.jpg"
+let Loin = "Loin.jpg"
+
+ function CToF(degree){
+   return degree*1.8+32
+   }
+function FToC(degree){
+   return (degree-32)/1.8
+   }
+   
+   
+   var cValueEl = document.getElementById("cValue");
+   cValueEl.onchange = function() {   
+       var cDegree = cValueEl.value;
+         document.getElementById("fValue").value = CToF(cDegree);
+   };
+   
+   var fValueEl = document.getElementById("fValue");
+   fValueEl.onchange = function() {   
+       var fDegree = fValueEl.value;
+         document.getElementById("cValue").value = CToF(fDegree);
+   };
+
+
+ 

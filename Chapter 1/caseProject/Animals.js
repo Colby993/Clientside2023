efe5c5a67d98f9434b3233403c018fa9361e947e
Colby993 <122412306+Colby993@users.noreply.github.com>
2023-01-31 21:34:49 -0600

    Update Animals.js

--- Chapter 1/caseProject/Animals.js
+++ Chapter 1/caseProject/Animals.js
@@ -4,25 +4,24 @@ let Alligator = "Alligator.jpg"
 let Cheetah = "Cheetah.jpg"
 let Loin = "Loin.jpg"
 
- function CToF(degree){
-   return degree*1.8+32
-   }
-function FToC(degree){
+
+
+function FahrenheitToCelsius(degree){
    return (degree-32)/1.8
    }
    
+   function CelsiusToFahrenheit(degree){
+   return degree*1.8+32
+   }
    
    var cValueEl = document.getElementById("cValue");
    cValueEl.onchange = function() {   
        var cDegree = cValueEl.value;
-         document.getElementById("fValue").value = CToF(cDegree);
+         document.getElementById("fValue").value = CelsiusToFahrenheit(cDegree);
    };
    
    var fValueEl = document.getElementById("fValue");
    fValueEl.onchange = function() {   
        var fDegree = fValueEl.value;
-         document.getElementById("cValue").value = CToF(fDegree);
+         document.getElementById("cValue").value = FahrenheitToCelsius(fDegree);
    };
-
-
- 

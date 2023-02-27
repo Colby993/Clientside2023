f23dc8d8353f8273fe97acdb01c06358ad7f74bf
Colby993 <122412306+Colby993@users.noreply.github.com>
2023-02-26 20:28:53 -0600

    Add files via upload

---
+++ Chapter4/caseproject/index.js
@@ -0,0 +1,52 @@
+window.onerror=function(msg, url,line) {
+   window.alert("Error: "+ msg+ "\nFile location: "+url+", line: "+line);
+   return true;
+}
+const email = document.querySelector("#email");
+const emailError = document.querySelector("#email_error");
+email.addEventListener("input", () => reset(emailError));
+const genderMale = document.querySelector("#gender_male");
+const genderFemale = document.querySelector("#gender_female");
+const genderError = document.querySelector("#gender_error");
+genderMale.addEventListener("input", () => reset(genderError));
+genderFemale.addEventListener("input", () => reset(genderError));
+function validate() {
+
+   let res = true;
+try{
+   if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value) === false) {
+      emailError.innerHTML = "invalid email address";
+      res = false;
+     
+   }
+}catch(err){
+   mailError.innerHTML =err;
+   emailError.innerHTML = "";
+   
+   
+   }
+   
+
+try{
+if ((genderMale.checked || genderFemale.checked) == false) {
+
+   genderError.innerHTML = "please select gender";
+   res = false;
+}
+}catch(er){
+   genderError.innerHTML =err;
+   genderError.innerHTML = "";
+
+}
+
+console.log(res);
+
+return res;
+
+}
+
+function reset(element) {
+
+   element.innerHTML = "";
+
+}
